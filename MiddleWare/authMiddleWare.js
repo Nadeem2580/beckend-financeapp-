c1b42d7701ddfe6a440ea4b23feb1bc0e95ad717
Muhammad Nadeem <nadeemismail2580@gmail.com>
2025-09-07 15:32:24 +0500

    jwt issue

--- MiddleWare/authMiddleWare.js
+++ MiddleWare/authMiddleWare.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 const authMiddleware = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        const isVerify = jwt.verify(token, process.env.PRIVATE_KEY);
+        const isVerify = jwt.verify(token, process.env.JWT_SECRET);
         if (isVerify?.id) {
             req.user = isVerify;
             next();
